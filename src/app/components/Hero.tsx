b1deeeb0572622d5f89b8e0e21a13b3e936c5b1e
genaileepro <daeseonglee7@gmail.com>
2025-04-17 15:49:47 +0900

    refactor: hero.tsx 일부 문구 수정정

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,11 +8,11 @@ export default function Hero() {
       
       <div className="container mx-auto px-4 z-10">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4 tracking-tight">
-          AI 전문가와 함께하는 <span className="text-emerald-500">교육 프로그램</span>
+          AI 전문가와 함께하는 <br/><span className="text-emerald-500">교육 프로그램</span>
         </h1>
         
         <p className="text-lg text-gray-300 mb-8 max-w-2xl mx-auto">
-          온라인, 오프라인 교육부터 맞춤형 AI 교육 및 개발 의뢰까지<br/>
+          온라인, 오프라인 교육부터 맞춤형 AI 교육 및 개발 의뢰까지
           다양한 방식으로 AI 기술을 습득하고 활용할 수 있습니다.
         </p>
         
